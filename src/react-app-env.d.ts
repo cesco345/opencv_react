6445efbe1c0ecdacc67bfab18a16b3a3fb5cd846
Francesco Piscani <fpiscani@gmail.com>
2024-11-06 21:09:48 -0500

    first commit

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,53 @@
+/// <reference types="react-scripts" />
+
+// Image declarations
+declare module "*.svg" {
+  import * as React from "react";
+  const SVGComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.ico" {
+  const src: string;
+  export default src;
+}
+
+// CSS modules
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.sass" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
